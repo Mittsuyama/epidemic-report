5eb6cd4fcf323583d9a572ecff9a42032fd0b694
mittsuyama <1462973856@qq.com>
2020-03-19 20:52:22 +0800

    home page tabbar

--- src/pages/home/_layout.tsx
+++ src/pages/home/_layout.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './bottombar.less';
+import { TabBar } from 'antd-mobile';
+import { history } from 'umi';
 
 export default (props: any) => {
   return (
     <div className="home-container">
       <div className="main-container">
-        <div>Home Page</div>
         <div>{props.children}</div>
       </div>
-      <div className="tab-bar-container">1</div>
+      <div className="tab-bar-container">{tabBarRender(props.route.path)}</div>
     </div>
+  );
+};
+
+const getLastPath = (path: string) => {
+  const pathList = path.split('/');
+  return pathList[pathList.length - 1];
+};
+
+const tabBarRender = (route: string) => {
+  const [selectMenu, setSelectMenu] = useState(getLastPath(route));
+  const tabBarMenu = [
+    { name: 'home', title: '主页', icon: 'icon-yidiandiantubiao04' },
+    { name: 'message', title: '消息', icon: 'icon-luntan' },
+    { name: 'me', title: '我的', icon: 'icon-kehufuwu' },
+  ];
+  const { Item } = TabBar;
+  return (
+    <TabBar unselectedTintColor="#333" tintColor="#66CCFF" barTintColor="white">
+      {tabBarMenu.map((item, index) => {
+        return (
+          <Item
+            title={item.title}
+            key={index}
+            icon={<i className={`iconfont ${item.icon}`} />}
+            selectedIcon={
+              <i
+                style={{ color: '#66CCFF' }}
+                className={`iconfont ${item.icon}`}
+              />
+            }
+            selected={item.name == selectMenu}
+            onPress={() => {
+              history.push(
+                `/home${item.name === 'home' ? '' : `/${item.name}`}`,
+              );
+              setSelectMenu(item.name);
+            }}
+          />
+        );
+      })}
+    </TabBar>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.less';
-import { Button, TabBar } from 'antd-mobile';
 
 export default () => {
   return <div>Index Page.</div>;
