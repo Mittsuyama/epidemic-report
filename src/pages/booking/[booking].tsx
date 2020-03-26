23c9d87e167a1714331ba8a124578a0528058ac9
mittsuyama <1462973856@qq.com>
2020-03-26 22:03:14 +0800

    form render, add DataPicker, picker

---
+++ src/pages/booking/[booking].tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import LIST from '@/utils/booking';
+import { List, InputItem, DatePicker, Picker, WhiteSpace, Button } from 'antd-mobile';
+// @ts-ignore
+import { createForm } from 'rc-form';
+
+const Booking = (props: any) => {
+  const { match } = props;
+  // @ts-ignore
+  const formList = LIST[match.params.booking] || [];
+
+  return (
+    <div className="booking-container">
+      <WhiteSpace />
+      {formRender(formList, props.form)}
+      <WhiteSpace />
+      <Button onClick={() => submit(props.form)}>提交</Button>
+    </div>
+  );
+};
+
+const submit = (form: any) => {
+  form.validateFields((error: any) => {
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(form.getFieldsValue());
+    }
+  })
+};
+
+const formRender = (list: any, form: any) => {
+  const { getFieldProps, getFieldError  } = form;
+
+  return (
+    <List>
+      {list.map((item: any) => {
+        if(item.type === 'input') {
+          return (
+            <InputItem
+              placeholder="请填写"
+              error={!!getFieldError(item.name)}
+              {...getFieldProps(item.name, {
+                rules: [
+                  { required: true, message: `请填写${item.title}` }
+                ],
+              })}
+              key={item.name}>{item.title}</InputItem>
+          );
+        } else if(item.type === 'time') {
+          return (
+            <DatePicker
+              key={item.name}
+              error={!!getFieldError(item.name)}
+              {...getFieldProps(item.name, {
+                initialValue: new Date(),
+                rules: [
+                  { required: true, message: `请选择${item.title}` }
+                ],
+              })
+              }
+            >
+              <List.Item arrow="horizontal">{item.title}</List.Item>
+            </DatePicker>
+          );
+        } else if(item.type === 'picker') {
+          return (
+            <Picker
+              key={item.name}
+              data={item.option}
+              cols={1}
+              {...getFieldProps(item.name, {
+                rules: [
+                  { required: true },
+                ],
+              })}
+              >
+              <List.Item arrow="horizontal">{item.title}</List.Item>
+            </Picker>
+          );
+        }
+      })}
+    </List>
+  );
+};
+
+export default createForm()(Booking);
--- src/utils/booking.ts
+++ src/utils/booking.ts
@@ -2,9 +2,20 @@ export default {
   classroom: [
     { type: 'input', name: 'name', title: '姓名' },
     { type: 'input', name: 'id', title: '学号' },
-    { type: 'input', name: 'building', title: '教学楼' },
+    {
+      type: 'picker',
+      name: 'building',
+      title: '教学楼',
+      option: [
+        { label: '正心楼', value: '正心楼' },
+        { label: '诚意楼', value: '诚意楼' },
+        { label: '明德楼', value: '明德楼' },
+        { label: '逸夫楼', value: '逸夫楼' },
+      ],
+    },
     { type: 'input', name: 'classroom', title: '教室编号' },
-    { type: 'input', name: 'time', title: '使用时间' },
+    { type: 'time', name: 'start_time', title: '开始时间' },
+    { type: 'time', name: 'end_time', title: '结束时间' },
   ],
   canteen: [],
   express: [],
