b388a9244d58a4fc851cba3906b7529a5b2f5c46
mittsuyama <1462973856@qq.com>
2020-03-30 22:45:10 +0800

    set report update function

--- src/pages/info/[info].tsx
+++ src/pages/info/[info].tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import INFO from '@/utils/constant/info';
 // @ts-ignore
 import Form from '@/components/form';
+import { queryPersonalInformationUpdate, queryPersonalReport, queryRoomReport} from '@/utils/api';
+
+let pageName = '';
 
 export default (props: any) => {
   const { match } = props;
+  pageName = match.params.info;
   // @ts-ignore
-  const formList = INFO[match.params.info] || [];
+  const formList = INFO[pageName] || [];
 
   return (
     <div className="info-container">
@@ -16,11 +20,32 @@ export default (props: any) => {
 };
 
 const submit = (form: any) => {
-  form.validateFields((error: any) => {
+  form.validateFields(async(error: any) => {
     if(error) {
       console.log(error);
     } else {
-      console.log(form.getFieldsValue());
+      const data = form.getFieldsValue();
+      const updateFunction: Function = getUpdateFunction();
+      await updateFunction();
     }
   })
 };
+
+const getUpdateFunction = () => {
+  let report: Function;
+  switch (pageName) {
+    case 'info':
+      report = queryPersonalInformationUpdate;
+      break;
+    case 'personal':
+      report = queryPersonalReport;
+      break;
+    case 'room':
+      report = queryRoomReport;
+      break;
+    default:
+      report = () => 0;
+  }
+  return report;
+};
+
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,3 +6,15 @@ export const queryMessageList = () => ajax('GET', 'api/message/list');
 // 获取消息详细
 export const queryMessageDetail = (id: string) =>
   ajax('GET', `api/message/detail/${id}`);
+
+// personal information update
+export const queryPersonalInformationUpdate = (params: any) =>
+  ajax('POST', 'api/info/info', params);
+
+// everyday personally reporting
+export const queryPersonalReport = (params: any) =>
+  ajax('POST', 'api/info/personal', params);
+
+// everyday dormitory reporting
+export const queryRoomReport = (params: any) =>
+  ajax('POST', 'api/info/dorm', params);
