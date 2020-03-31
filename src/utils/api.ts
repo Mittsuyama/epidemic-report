21d5858451d4779ad945ab2d06091ee3bad319e3
mittsuyama <1462973856@qq.com>
2020-03-31 17:22:00 +0800

    add booking report api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,6 +7,22 @@ export const queryMessageList = () => ajax('GET', 'api/message/list');
 export const queryMessageDetail = (id: string) =>
   ajax('GET', `api/message/detail/${id}`);
 
+// classroom booking
+export const queryClassroomBooking = (params: any) =>
+  ajax('POST', 'api/booking/classroom', params);
+
+// canteen booking
+export const queryCanteenBooking = (params: any) =>
+  ajax('POST', 'api/booking/canteen', params);
+
+// supermarket booking
+export const querySupermarketBooking = (params: any) =>
+  ajax('POST', 'api/booking/supermarket', params);
+
+// express booking
+export const queryExpressBooking = (params: any) =>
+  ajax('POST', 'api/booking/express', params);
+
 // personal information update
 export const queryPersonalInformationUpdate = (params: any) =>
   ajax('POST', 'api/info/info', params);
